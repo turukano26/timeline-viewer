cc82a5f56cf0f47831380774d79e209f6277c6e3
joey love <joeylove26@gmail.com>
2022-06-22 21:03:56 -0400

    modularized the attribute fields and their creation

--- dataInput.py
+++ dataInput.py
@@ -31,30 +31,93 @@ input_type['values'] = ('Person', 'Country')
 input_type.state(["readonly"])
 input_type.grid(row=0, column=0)
 
+class Lotfi(Entry):
+    def __init__(self, master, allowed_digits, **kwargs):
+        self.allowed_digits = allowed_digits
+        self.var = StringVar()
+        Entry.__init__(self, master, textvariable=self.var, **kwargs)
+        self.old_value = ''
+        self.var.trace('w', self.check)
+        self.get, self.set = self.var.get, self.var.set
+
+    def check(self, *args):
+        if self.get().isdigit() and len(self.get()) <= self.allowed_digits: 
+            # the current value is only digits; allow this
+            self.old_value = self.get()
+        else:
+            # there's non-digit characters in the input; reject this 
+            self.set(self.old_value)
+
+        
+
+class AttributeField:
+    def __init__(self, name, row_num, parent_frame):
+        self.name = name
+        self.row = row_num
+
+        self.label = Label(parent_frame, text=name)
+        self.label.grid(row=row_num, column=0)
+
+
+class StringField(AttributeField):
+    def __init__(self, name, row_num, parent_frame):
+        self.entry = Entry(parent_frame)
+        self.entry.grid(row=row_num, column=1)
+        
+        AttributeField.__init__(self, name, row_num, parent_frame)
+
+    def get_data(self):
+        return self.entry.get()
+
+
+class DateField(AttributeField):
+    def __init__(self, name, row_num, parent_frame):
+        self.entry_year = Lotfi(parent_frame, 5)
+        self.entry_year.grid(row=row_num, column=1)
+
+        self.entry_month = Lotfi(parent_frame, 2)
+        self.entry_month.grid(row=row_num, column=2)
+
+        self.entry_day = Lotfi(parent_frame, 2)
+        self.entry_day.grid(row=row_num, column=3)
+        
+        AttributeField.__init__(self, name, row_num, parent_frame)
+
+    def get_data(self):
+        rtn_str = self.entry_year.get()
+
+        if self.entry_month.get() == '': return rtn_str
+        rtn_str += '-' + self.entry_month.get()
+
+        if self.entry_day.get() == '': return rtn_str
+        return rtn_str + '-' + self.entry_day.get()
+    
+
+
+
+
 
 #defines person input window
 def create_person_input():
 
     def submitForm():
-        output = Label(root, text=name.get() + birthDate.get())
+        longStr = ''
+        for entry in createdFields:
+            longStr += entry.get_data() + "\n"
+        output = Label(root, text=longStr)
         output.grid(row=4, column=0)
 
-    titleLabel = Label(person_input, text="Insert Person")
-    titleLabel.grid(row=0)
+    attributes = [["Name", StringField],
+                 ["Born", DateField],
+                 ["Died", DateField]]
 
-    name = Entry(person_input)
-    name.grid(row=1)
+    createdFields = []
 
-    birthDate = Entry(person_input)
-    birthDate.grid(row=2, column=0)
-
-    deathDate = Entry(person_input)
-    deathDate.grid(row=2, column=1)
+    for i, att in enumerate(attributes):
+        createdFields.append(att[1](att[0], i, person_input))
 
     button1 = Button(person_input, text='Okay', command=submitForm)
-    button1.grid(row=3)
-
-    
+    button1.grid(row=i+1)
 
     person_input.grid(row=1, column=0)
 
@@ -63,23 +126,23 @@ def create_person_input():
 def create_country_input():
 
     def submitForm():
-        output = Label(root, text=name.get() + foundation_date.get())
-        output.grid(row=4, column=1)
+        longStr = ''
+        for entry in createdFields:
+            longStr += entry.get_data() + "\n"
+        output = Label(root, text=longStr)
+        output.grid(row=4, column=0)
 
-    titleLabel = Label(country_input, text="Insert Country")
-    titleLabel.grid(row=0)
+    attributes = [["Name", StringField],
+                 ["Founded", DateField],
+                 ["Fell", DateField]]
 
-    name = Entry(country_input)
-    name.grid(row=1)
+    createdFields = []
 
-    foundation_date = Entry(country_input)
-    foundation_date.grid(row=2, column=0)
-
-    destruction_date = Entry(country_input)
-    destruction_date.grid(row=3, column=1)
+    for i, att in enumerate(attributes):
+        createdFields.append(att[1](att[0], i, country_input))
 
     button1 = Button(country_input, text='Okay', command=submitForm)
-    button1.grid(row=4)
+    button1.grid(row=i+1)
 
     country_input.grid(row=1, column=0)
 
